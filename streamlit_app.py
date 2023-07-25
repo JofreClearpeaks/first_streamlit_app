f1b8cea9c2752edba94c1ab76e4a86b813b2d484
JofreClearpeaks <138578113+JofreClearpeaks@users.noreply.github.com>
2023-07-25 12:38:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.subtitle('Breakfast Menu')
+streamlit.title('Breakfast Menu')
 'Omega 3 & Blueberry Oatmeal'
 'Kale, Spinach & Tocket Smoothie'
 'Hard-Boiled Free-Range Egg'
